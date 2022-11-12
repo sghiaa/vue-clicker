de80a620228cf3e3ce049f7ce9d5e3c472885d9f
James Martineau <github@neau.me>
2022-11-11 21:32:30 -0500

    Cleaned up test name and descriptions

--- e2e/vue.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("visits the app root url", async ({ page }) => {
-  await page.goto("/");
-  await new Promise(resolve => setTimeout(resolve, 2000)); // Just to give you a chance to see that it's actually loading
-  await expect(page.locator("button.mainButton")).toHaveText("Click here");
-});
