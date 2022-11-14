b037c08a67ab2a35b26a72988040221c53e96058
Alex Sghia-Hughes <alex@Alexs-MacBook-Pro.local>
2022-11-14 16:16:45 -0500

    move image to its own component

---
+++ src/components/CountImage.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+import { state } from '../services/GameState'
+
+export default {
+  data() {
+    return {
+      state,
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="countImage">
+    <img
+      v-if="state.count <= 1000"
+      alt="Water droplet"
+      class=""
+      src="@/assets/water-droplet.jpg"
+      width="125"
+      height="125"
+    />
+    <img
+      v-if="state.count > 1000 && state.count <= 10000"
+      alt="Lightning strike"
+      class=""
+      src="@/assets/lightning-strike.jpg"
+      width="125"
+      height="125"
+    />
+    <img
+      v-if="state.count > 10000 && state.count <= 100000"
+      alt="Dandylion"
+      class=""
+      src="@/assets/dandylion.jpg"
+      width="125"
+      height="125"
+    />
+    <img
+      v-if="state.count > 100000"
+      alt="Tree"
+      class=""
+      src="@/assets/tree.jpg"
+      width="125"
+      height="125"
+    />
+  </div>
+</template>
---
+++ src/views/ImageView.vue
@@ -0,0 +1,17 @@
+
+<script lang="ts">
+  import Countimage from "../components/CountImage.vue"
+</script>
+<template>
+  <CountImage />
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
